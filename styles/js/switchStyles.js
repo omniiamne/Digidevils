8e12c7679a434540bd8c4c9b439e91026861e2aa
omniiamne <89515958152@mail.ru>
2026-03-14 22:06:49 +0500

    Update switchStyles.js

--- styles/js/switchStyles.js
+++ styles/js/switchStyles.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
     const styleLink = document.getElementById("theme-style");
-    const savedTheme = localStorage.getItem("theme") || "light";
+    const savedTheme = localStorage.getItem("theme") || "charadex";
 
-    // Применяем сохранённую тему
+    
     applyTheme(savedTheme);
 
-    // Вешаем обработчики на кнопки выбора темы
+    
     document.querySelectorAll("[data-theme]").forEach(btn => {
         btn.addEventListener("click", () => {
             const theme = btn.getAttribute("data-theme");
