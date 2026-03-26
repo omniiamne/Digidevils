8ef4934f5c2e2312a2fd5aefc2f6be46d6a34e6e
omniiamne <89515958152@mail.ru>
2026-03-26 15:28:18 +0500

    Update config.js

--- styles/js/config.js
+++ styles/js/config.js
@@ -44,11 +44,11 @@ charadex.sheet = {
   options: {
 
     designTypes: ['All', 'Official Design', 'Guest Design', 'MYO Slot', 'MYO Design'],
-    statuses: ['All', 'Resell', 'Trade', 'Gift', 'Voided', 'For Sale', 'Purchased'],
-    rarity: ['All', 'Common', 'Uncommon', 'Rare', 'Very Rare', 'Legendary'],
-    species: ['All', 'Dog', 'Cat', 'Bunny'],
+    statuses: ['All', 'Open for trade', 'Open for sale', 'Open for gift art', 'Open for gift writing', 'NFO', 'Voided'],
+    rarity: ['All', 'Common', 'Uncommon', 'Rare', 'Corrupted'],
+    species: ['All', 'Digidevil'],
     itemTypes: ['All', 'Currency', 'MYO Slot', 'Pet', 'Trait', 'Misc'],
-    traitTypes: ['All', 'Ears', 'Eyes', 'Body', 'Limbs', 'Tails', 'Misc', 'Mutations']
+    traitTypes: ['All', 'Ears', 'Eyes', 'Body', 'Limbs', 'Tails', 'Misc']
 
   }
 
